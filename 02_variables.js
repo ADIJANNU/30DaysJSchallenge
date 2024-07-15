3ad9d9f165fefa44f72fc17082f7c7437ba6ab94
Adithya Jannu <adijannu09@gmail.com>
2024-07-15 07:32:42 +0000

    tasks done

---
+++ 02_variables.js
@@ -0,0 +1,17 @@
+let num2 = 18;
+console.log(`typeof ${num2} = ${num2} `);
+
+let str2 = 'Adi';
+console.log(`typeof ${str2} = ${str2}`);
+
+let bool2 = false;
+console.log(typeof(bool2) + " " +  bool2);
+
+let obj1 = {
+    name : 'Aditya',
+    srn: 802
+}
+console.log(typeof obj1, obj1);
+
+let arr1 = ['virat',18]
+console.log(typeof(arr1), arr1);
